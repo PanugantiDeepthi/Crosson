ab4e364fde365b7d3b94c3e5ade52b8fcdcf55e0
DeepthiPanuganti <153904743+DeepthiPanuganti@users.noreply.github.com>
2024-05-28 14:15:41 +0530

    created slider component

--- Crosson/src/App.jsx
+++ Crosson/src/App.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 import { experience } from './typography/typography'
 import './typography/typography.scss';
 import Header from './components/Header/Header';
+import Slider from './components/Slider/slider';
 
 
 const App = () => {
   return (
-    <div><Header /></div>
+    <>
+    <div><Header/></div>
+    <div><Slider/></div>
+
+    </>
+    
   )
 }
 
